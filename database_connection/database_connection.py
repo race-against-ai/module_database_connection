641d592676a69a186848a2264fe5e13990f985bc
Philipp <112952535+PhilippTrashman@users.noreply.github.com>
2023-09-21 13:20:37 +0200

    added worker thread for data queue

--- database_connection/database_connection.py
+++ database_connection/database_connection.py
@@ -1,11 +1,23 @@
-from __future__ import annotations
-
 import json
 import requests
 import pynng
 import os
 import sys
 from pathlib import Path
+import threading
+import queue
+import time
+
+
+def is_internet_available():
+    try:
+        # Attempt to send an HTTP GET request to a well-known website
+        response = requests.get("http://www.google.com", timeout=5)
+        response.raise_for_status()  # Raise an exception for HTTP errors
+        return True  # Internet connection is available
+    except requests.RequestException:
+        pass
+    return False  # Internet connection is not available
 
 
 def resource_path() -> Path:
@@ -82,6 +94,16 @@ class DatabaseConnection:
         self.__config = json.load(open((resource_path() / "database_connection_config.json"), "r"))
         self.__url_prefix = "https://raaidatabaseapi.azurewebsites.net/api/"
 
+        self.__internet_connection = is_internet_available()
+        self.__api_queue = queue.Queue()
+        self.__unsent_data = {}
+
+        self.__api_worker_thread = threading.Thread(target=self.__api_worker)
+        self.__api_worker_thread.deamon = True
+
+        self.__unsent_data_thread = threading.Thread(target=self.__send_unsent_data_worker)
+        self.__unsent_data_thread.deamon = True
+
         publisher_address = self.__config["pynng"]["publishers"]["data_publisher"]["address"]
         self.__data_publisher = pynng.Pub0(listen=publisher_address)
 
@@ -92,14 +114,36 @@ class DatabaseConnection:
             self.__time_tracking_subscriber.subscribe(entry)
         self.__time_tracking_subscriber.dial(time_tracking_address, block=False)
 
-        self.__current_driver = {
-            "id": None,
-            "name": None
-        }
+        self.__current_driver = {}
+        self.__current_convention = 2
+
         self.__current_lap = None
         self.__current_lap_valid = True
 
-    def receive_time_tracking(self):
+    def start(self):
+        self.__api_worker_thread.start()
+        while True:
+            if self.__unsent_data:
+                self.__unsent_data_thread.start()
+            self.__receive_time_tracking()
+
+    def __assign_new_driver(self, driver_id: int, driver_name: str):
+        self.__current_driver["id"] = driver_id
+        self.__current_driver["name"] = driver_name
+
+    def __change_convention(self, convention_id: int):
+        self.__current_convention = convention_id
+
+    def __reset_lap(self):
+        self.__current_lap = {
+            "sector1": None,
+            "sector2": None,
+            "sector3": None,
+            "time": None
+        }
+        self.__current_lap_valid = True
+
+    def __receive_time_tracking(self):
         data_recv = receive_data_mod(self.__time_tracking_subscriber)
         if data_recv is not None:
             topic: str = data_recv[1]
@@ -110,22 +154,100 @@ class DatabaseConnection:
                 print("lap_start")
                 print(data, "\n")
                 if self.__current_lap is None:
-                    self.__current_lap = {
-                        "sector1": None,
-                        "sector2": None,
-                        "sector3": None,
-                        "time": None
-                    }
+                    self.__reset_lap()
+                else:
+                    url_suffix = self.__functions["PostDriverTime"]["url"]
+                    header = self.__functions["PostDriverTime"]["header"]
+                    method = self.__functions["PostDriverTime"]["method"]
+                    url = self.__url_prefix + url_suffix
+                    for entry, value in self.__current_lap.items():
+                        if value is None:
+                            self.__current_lap_valid = False
+                            break
+                    if self.__current_lap_valid and self.__current_driver:
+                        data = {
+                            "id": self.__current_driver["id"],
+                            "lap_time": self.__current_lap["time"],
+                            "sector1": self.__current_lap["sector1"],
+                            "sector2": self.__current_lap["sector2"],
+                            "sector3": self.__current_lap["sector3"],
+                            "convention": self.__current_convention
+                        }
+                        if self.__current_driver["id"] is not None:
+                            self.__api_queue.put((url, data, header, method))
+
+                    self.__reset_lap()
 
             elif topic == "sector_finished":
                 print(data, "\n")
                 sector = data["sector_number"]
-                time = data["sector_time"]
+                lap_time = data["sector_time"]
                 valid = data["sector_valid"]
                 if self.__current_lap_valid:
                     if valid:
-                        self.__current_lap[f"sector{sector}"] = time
-                    else:
-                        self.__current_lap_valid = valid
+                        self.__current_lap[f"sector{sector}"] = lap_time
             elif topic == "lap_finished":
                 print("lap_finished")
+                lap_time = data["lap_time"]
+                valid = data["lap_valid"]
+                if valid:
+                    self.__current_lap["time"] = lap_time
+
+    def __retry_internet_connection(self):
+        if not self.__internet_connection:
+            self.__internet_connection = is_internet_available()
+
+    def __api_worker(self):
+        while True:
+            try:
+                url, data, headers, method = self.__api_queue.get()
+
+                response = send_request(url, data, headers, method)
+
+                if self.__internet_connection:
+                    response = send_request(url, data, headers, method)
+                    if response.status_code == 200:
+                        print(f"API request successful: {response.status_code}")
+
+                    else:
+                        print(f"API request failed: {response.status_code}")
+                        print(f"                    {response.text}")
+                else:
+                    identifier = str(time.time())
+                    self.__unsent_data[identifier] = {
+                        "url": url,
+                        "data": data,
+                        "headers": headers,
+                        "method": method
+                    }
+                    print(f"API request failed: {response.status_code}")
+                    print(f"                    {response.text}")
+                    print("Data saved for later transmission")
+
+                self.__api_queue.task_done()
+
+            except Exception as e:
+                print(e)
+                print("Error while sending API request")
+
+    def __send_unsent_data_worker(self):
+        if is_internet_available():
+            for identifier, data in self.__unsent_data.items():
+                url = data["url"]
+                data = data["data"]
+                headers = data["headers"]
+                method = data["method"]
+
+                response = send_request(url, data, headers, method)
+
+                if response.status_code == 200:
+                    print(f"API request successful: {response.status_code}")
+                    del self.__unsent_data[identifier]
+
+                else:
+                    print(f"API request failed: {response.status_code}")
+                    print(f"                    {response.text}")
+                    break
+
+        else:
+            print("No internet connection available")
--- database_connection/database_overlay.py
+++ database_connection/database_overlay.py
@@ -1,13 +1,75 @@
 import tkinter as tk
 import requests
 
+# Colors
+COOLGRAY = '#3B3730'
+SAGE = '#9D9480'
+NUDE = '#8C7460'
+MONOBLACK = '#171717'
+RED = '#58181F'
+WHITE = '#FDFDFD'
+
 
 class Window:
-    def __int__(self):
+    def __init__(self):
         self.__root = tk.Tk()
-        self.__root.title("RAAI Database")
+        self.__root.wm_title("RAAI Database")
         self.__root.geometry("800x600")
         self.__root.resizable(False, False)
-        self.__root.configure(bg="white")
+        self.__root.configure(bg=MONOBLACK)
+
+        self.__driver_data = {}
+        self.__driver_time_data = {}
+        self.__convention_data = {}
+
+        self.__setup_frames()
+
+    def __setup_frames(self):
+        self.__main_frame = tk.Frame(self.__root, bg=MONOBLACK)
+        self.__main_frame.pack(fill=tk.BOTH, expand=True)
+
+        self.__left_side = tk.Frame(self.__main_frame, bg=SAGE)
+        self.__left_side.grid(row=0, column=0, sticky="nsew")
+
+        self.__right_side = tk.Frame(self.__main_frame, bg=SAGE)
+        self.__right_side.grid(row=0, column=1, sticky="nsew")
+
+        self.__data_frame = tk.Frame(self.__right_side, bg=COOLGRAY)
+        self.__data_frame.pack(fill=tk.BOTH, expand=True)
+
+        self.__bottom_bar_frame = tk.Frame(self.__data_frame, bg=SAGE)
+        self.__bottom_bar_frame.pack(side=tk.BOTTOM, fill=tk.X, expand=False)
+
+        self.__data_frame.grid_rowconfigure(1, weight=1)
+
+        self.__main_frame.grid_columnconfigure(0, weight=1)
+        self.__main_frame.grid_columnconfigure(1, weight=1)
+        self.__main_frame.grid_rowconfigure(0, weight=1)
+
+    def __setup_buttons(self):
+        self.__bottom_bar_frame = tk.Frame(self.__data_frame, bg=SAGE)
+        self.__bottom_bar_frame.pack(side=tk.BOTTOM, fill=tk.X, expand=False)
+
+        self.__data_frame.grid_rowconfigure(1, weight=1)
+
+        button1 = tk.Button(self.__bottom_bar_frame, text="Button 1")
+        button1.pack(side=tk.LEFT)
+
+        button2 = tk.Button(self.__bottom_bar_frame, text="Button 2")
+        button2.pack(side=tk.LEFT)
+
+        button3 = tk.Button(self.__bottom_bar_frame, text="Button 3")
+        button3.pack(side=tk.LEFT)
+
+    def run(self):
+        self.__root.update()
+        while True:
+            self.__root.update_idletasks()
+            self.__root.update()
+            # self.__update_data()
+            # self.__update_buttons()
 
 
+if __name__ == "__main__":
+    window = Window()
+    window.run()
